2ca824708fe0ab1924b6172a19a8e668f79e9419
AvaneeshKhandekar <avaneesh.khandekar@gmail.com>
2025-06-12 16:20:28 -0400

    fix

--- src/components/sections/recommendations.js
+++ src/components/sections/recommendations.js
@@ -19,7 +19,7 @@ const StyledRecommendationsSection = styled.section`
 
     .recommendation-grid {
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(325px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
     gap: 30px;
     margin-top: 50px;
     justify-content: center;
